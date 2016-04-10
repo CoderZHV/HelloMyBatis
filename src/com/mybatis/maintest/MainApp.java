35589ffe7745fe5f62e091a957e28cd53419d399
isoft-v <HW@DESKTOP-HA1M3SR>
2016-04-10 10:16:23 +0800

    add "delete select insert update "

--- src/com/mybatis/maintest/MainApp.java
+++ src/com/mybatis/maintest/MainApp.java
@@ -1,6 +1,7 @@
 package com.mybatis.maintest;
 
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,15 +14,49 @@ import com.mybatis.pojo.User;
 public class MainApp {
 	
 	@Test
-	public void firstDemo() {
+	public void addUser() {
 		
 		SqlSession session = MyBatisUtils.getSession();
 		
-		String arg = "com.mybatis.map.userMap.getUser";
-		int num = session.insert(arg, new User(-1,"jane", 13));
+		String arg = "com.mybatis.map.userMap.addUser";
+		int num = session.insert(arg, new User("jane", 18));
+		session.commit();
 		
 		System.out.println(num);
 		
+	}
+	
+	@Test
+	public void deleteUser(){
+		
+		SqlSession session = MyBatisUtils.getSession();
+		
+		String arg = "com.mybatis.map.userMap.deleteUser";
+		int num = session.delete(arg, "jane");
+		session.commit();
+		System.out.println(num);
+		
+	}
+	
+	@Test
+	public void updateUser(){
+		
+		SqlSession session = MyBatisUtils.getSession();
+		
+		String arg = "com.mybatis.map.userMap.updateUser";
+		int num = session.update(arg, new User(2, "davi", 22));
+		session.commit();
+		System.out.println(num);
+		
+	}
+	
+	@Test
+	public void getAllUser(){
+		SqlSession session = MyBatisUtils.getSession();
+		
+		String arg = "com.mybatis.map.userMap.getAllUser";
+		List<User> list = session.selectList(arg);
+		System.out.println(list);
 	}
 	
 }
--- src/com/mybatis/pojo/User.java
+++ src/com/mybatis/pojo/User.java
@@ -41,6 +41,11 @@ public class User {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
 	
 	
 	
